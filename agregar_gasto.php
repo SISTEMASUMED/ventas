543553d480c4241eacfbd81adf117bcd7c276eea
betitotapia <62372677+betitotapia@users.noreply.github.com>
2024-05-02 09:46:56 -0600

    update agregar_gastp.php

--- agregar_gasto.php
+++ agregar_gasto.php
@@ -111,15 +111,15 @@ include("head.php");?>
 
         <div class="form-group row">    <!-- cuarto group -->
          
-          <label for="subtotal" class="col-sm-1 control-label">Subtotal:</label>
+          <label for="subtotal" class="col-sm-1 control-label">Subtotal: $</label>
             <div class="col-md-3">
               <input type="text" id="subtotal" name="subtotal" readonly class="form-control input-sm">
             </div>
-          <label for="iva" class="col-sm-1 control-label">IVA:</label>
+          <label for="iva" class="col-sm-1 control-label">IVA: $</label>
             <div class="col-md-2">
               <input type="text" id="iva" name="iva" readonly class="form-control input-sm">
             </div>
-          <label for="total" class="col-sm-1 control-label">Total:</label>
+          <label for="total" class="col-sm-1 control-label">Total: $</label>
             <div class="col-md-2">
               <input type="text" id="total" name="total" readonly class="form-control input-sm">
             </div>
@@ -168,9 +168,9 @@ include("head.php");?>
       document.getElementById('proveedor').value = xmlDoc.getElementsByTagName('cfdi:Emisor')[0].getAttribute('Nombre');
       document.getElementById('uuid').value = xmlDoc.getElementsByTagName('tfd:TimbreFiscalDigital')[0].getAttribute('UUID');
       document.getElementById('folio').value = xmlDoc.getElementsByTagName('cfdi:Comprobante')[0].getAttribute('Folio');
-      document.getElementById('subtotal').value = '$'+ xmlDoc.getElementsByTagName('cfdi:Comprobante')[0].getAttribute('SubTotal');
-      document.getElementById('iva').value = '$'+ xmlDoc.getElementsByTagName('cfdi:Traslado')[0].getAttribute('Importe');
-      document.getElementById('total').value = '$'+ xmlDoc.getElementsByTagName('cfdi:Comprobante')[0].getAttribute('Total');
+      document.getElementById('subtotal').value = xmlDoc.getElementsByTagName('cfdi:Comprobante')[0].getAttribute('SubTotal');
+      document.getElementById('iva').value = xmlDoc.getElementsByTagName('cfdi:Traslado')[0].getAttribute('Importe');
+      document.getElementById('total').value = xmlDoc.getElementsByTagName('cfdi:Comprobante')[0].getAttribute('Total');
     };
     reader.readAsText(file);
   }
